d4489938d958c5ecb8bafe0e63290804ca872e1d
1herna <139081327+1herna@users.noreply.github.com>
2025-05-17 23:30:18 +0100

    Add files via upload

--- app1.py
+++ app1.py
@@ -5,10 +5,10 @@ from PIL import Image
 # Carregar imagem corretamente
 img = Image.open("Imagem/WhatsApp Image 2024-09-29 at 02.22.00.jpeg")
 
-# Configurar página
+# Configurar a página
 st.set_page_config(page_title="PYGEOPLOT", page_icon=img, layout="wide")
 
-# Mostrar logotipo
+# Exibir logotipo no topo
 st.image(img, use_container_width=True, caption="PYGEOPLOT")
 
 # Menu lateral
@@ -37,31 +37,45 @@ with st.sidebar:
         default_index=0,
     )
 
-# Executar a página correspondente
+# Navegação para páginas
 if escolha == "Importação":
     import importacao
+    import importlib
+    importlib.reload(importacao)
     importacao.app()
 
 elif escolha == "Visualização":
-    import visualizacao
-    visualizacao.app()
+    import plotagem
+    import importlib
+    importlib.reload(plotagem)
+    plotagem.app()
 
 elif escolha == "Estatísticas":
     import estatistica
+    import importlib
+    importlib.reload(estatistica)
     estatistica.app()
 
 elif escolha == "Classificação Litológica":
     import classificacao
+    import importlib
+    importlib.reload(classificacao)
     classificacao.app()
 
 elif escolha == "Cálculo Petrofísico":
     import calculopetrofisico
+    import importlib
+    importlib.reload(calculopetrofisico)
     calculopetrofisico.app()
 
 elif escolha == "Conversão de Dados":
     import conversao
+    import importlib
+    importlib.reload(conversao)
     conversao.app()
 
 elif escolha == "Autor do Aplicativo":
     import autor
+    import importlib
+    importlib.reload(autor)
     autor.app()
